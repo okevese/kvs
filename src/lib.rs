a18a3e63d7017366a0731a6c38f928f20bf5ec2f
Oke Vese <okevese@gmail.com>
2019-10-06 23:11:41 +0100

    set up project and made tests pass with --no-run flag

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub struct KvStore {
+
+}
+
+impl KvStore {
+	pub fn new() -> KvStore {
+		unimplemented!();
+	}
+	
+	pub fn set(&mut self, _key: String, _value: String)  {
+		unimplemented!();
+	}
+
+	pub fn get(&self, _key: String) -> Option<String> {
+		unimplemented!();
+	}
+
+	pub fn remove(&self, _key: String) {
+		unimplemented!();
+	}
+}
