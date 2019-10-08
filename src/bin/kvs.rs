dc91020bd3b16a43a7464bbae5b837a84b39f9c7
Oke Vese <okevese@gmail.com>
2019-10-09 00:24:00 +0100

    fixed the, called Option::unwrap() on a None value error. It was in the YAML file.

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -3,13 +3,26 @@ extern crate clap;
 use clap::App;
 
 fn main() {
-    let yaml = load_yaml!("../../cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
-
+    let _yaml = load_yaml!("../../cli.yml");
+    let matches = App::from_yaml(_yaml).get_matches();
+    
     let config = matches.value_of("config").unwrap_or("default.conf");
     println!("Value for config: {}", config);
 
     println!("Using input file: {}", matches.value_of("INPUT").unwrap());
 
-   
+    match matches.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    if let Some(matches) = matches.subcommand_matches("test") {
+        if matches.is_present("debug") {
+            println!("Printing debug info...");
+        } else {
+            println!("Print normally...");
+        }
+    }
 }
