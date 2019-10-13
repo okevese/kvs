6df08349a27b841886f7e0c9363f9926a942e4f1
Oke Vese <okevese@gmail.com>
2019-10-13 16:23:43 +0100

    made first test case pass. Needs refactoring of panic to Result

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,28 +1,22 @@
-#[macro_use]
+
 extern crate clap;
-use clap::App;
+use clap::{ Arg, App };
 
 fn main() {
-    let _yaml = load_yaml!("../../cli.yml");
-    let matches = App::from_yaml(_yaml).get_matches();
-    
-    let config = matches.value_of("config").unwrap_or("default.conf");
-    println!("Value for config: {}", config);
+    let matches = App::new("kvs")
+        .version("1.0.0")
+        .author("Oke Vese <okevese@gmail.com")
+        .about("a key value store")
+        .arg(
+            Arg::with_name("argument")
+                .help("some argument to the binary")
+        )
+        .get_matches();
+    println!("{:?}", matches.usage());
 
-    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
-
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Print normally...");
-        }
+    if let Some(arg) = matches.value_of("argument") {
+        println!("An arg was passed in: {}", arg);
+    } else {
+        panic!(2);
     }
 }
