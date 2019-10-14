e4b94d7eb36d1814b6e7e92dcad884c8f62404bf
Oke Vese <okevese@gmail.com>
2019-10-14 20:36:38 +0100

    changed version

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,22 +1,36 @@
 
 extern crate clap;
 use clap::{ Arg, App };
+use kvs::*;
 
 fn main() {
     let matches = App::new("kvs")
-        .version("1.0.0")
+        .version("0.1.0")
         .author("Oke Vese <okevese@gmail.com")
         .about("a key value store")
         .arg(
             Arg::with_name("argument")
                 .help("some argument to the binary")
         )
+        .arg(
+            Arg::with_name("set")
+            .multiple(true)
+            .takes_value(true)
+            .help("get a saved key") 
+        )
         .get_matches();
-    println!("{:?}", matches.usage());
 
+    println!("{:?}", matches.usage());
+    
     if let Some(arg) = matches.value_of("argument") {
         println!("An arg was passed in: {}", arg);
     } else {
         panic!(2);
     }
+
+    if let Some(key) = matches.value_of("set") {
+        println!("unimplemented");
+        panic!(2);
+    }
 }
+
