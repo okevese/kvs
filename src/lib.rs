c7b1b997c899fd346f212935f50ab0cba0ac17ea
oke vese <okevese@gmail.com>
2019-11-19 22:00:24 +0100

    added comments'

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
- 
+
 use std::collections::HashMap;
 
 #[derive(Debug, PartialEq)] 
@@ -7,20 +7,23 @@ pub struct KvStore {
 }
 
 impl KvStore {
+	// Create a new kvStore hashmap
 	pub fn new() -> KvStore {
 		KvStore {
 			entry: HashMap::new(),
 		}
 	}
-	
+	// Inserts `key` in the  KvStore hashmap
 	pub fn set(&mut self, _key: String, _value: String)  {
 		self.entry.insert(_key, _value);
 	}
 
+	// Gets `key` from the KvStore hashmap
 	pub fn get(&mut self, _key: String) -> Option<String> {
 		self.entry.get(&_key).cloned()
 	}
 
+	// Removes `key` from the kvStore hashmap
 	pub fn remove(&mut self, _key: String) {
 		self.entry.remove(&_key);
 	}
