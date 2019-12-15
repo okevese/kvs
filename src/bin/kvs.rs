984a70340640ae6080f4cfbbdc2043e30def17dd
oke vese <okevese@gmail.com>
2019-12-15 07:56:22 +0100

    minnor changes to format

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,6 +1,5 @@
-
 extern crate clap;
-use clap::{ Arg, App };
+use clap::{App, Arg};
 use kvs::*;
 
 fn main() {
@@ -11,77 +10,59 @@ fn main() {
         .subcommand(
             App::new("set")
                 .about("Sets new key values")
-                .arg(
-                    Arg::with_name("key")
-                        .help("Key to be added")
-                        .required(true)
-                )
+                .arg(Arg::with_name("key").help("Key to be added").required(true))
                 .arg(
                     Arg::with_name("value")
                         .help("Value for the attached key")
-                        .required(true)
-                )
+                        .required(true),
+                ),
         )
         .subcommand(
             App::new("get")
                 .about("Gets a stored key value")
-                .arg(
-                    Arg::with_name("key")
-                        .help("Key to be added")
-                        .required(true)
-                )
-    
+                .arg(Arg::with_name("key").help("Key to be added").required(true)),
         )
         .subcommand(
-            App::new("rm")
-                .about("Deletes a stored key value")
-                .arg(
-                    Arg::with_name("key")
-                        .help("Key to be deleted")
-                        .required(true)
-                )
-    
+            App::new("rm").about("Deletes a stored key value").arg(
+                Arg::with_name("key")
+                    .help("Key to be deleted")
+                    .required(true),
+            ),
         )
         .get_matches();
 
     match matches.subcommand() {
         ("set", Some(_set_matches)) => {
             to_set(_set_matches.value_of("key"), _set_matches.value_of("value"));
-        },
+        }
         ("get", Some(_get_matches)) => {
             to_get(_get_matches.value_of("key"));
-        },
+        }
         ("rm", Some(_rm_matches)) => {
             to_remove(_rm_matches.value_of("key"));
-        },
+        }
         ("", None) => {
             panic!(2);
-        },
+        }
         _ => unreachable!(),
     }
-
 }
-
 
 fn to_set(_key: Option<&str>, _value: Option<&str>) {
     match _key {
-        Some(_key) => {
-            match _value {
-                Some(_value) => {
-                    let mut _store = KvStore::new();
-                    _store.set(_key.to_owned(), _value.to_owned());
-                    println!("{:?}", _store);
-                    _store.set("profession".to_owned(), "programmer".to_owned());
-                    println!("{:?}", _store);
-                },
-                None => unimplemented!()
+        Some(_key) => match _value {
+            Some(_value) => {
+                let mut _store = KvStore::new();
+                _store.set(_key.to_owned(), _value.to_owned());
+                println!("{:?}", _store);
+                _store.set("profession".to_owned(), "programmer".to_owned());
+                println!("{:?}", _store);
             }
+            None => unimplemented!(),
         },
-        None => unimplemented!()
-        
+        None => unimplemented!(),
     }
 }
-
 
 fn to_get(_key: Option<&str>) {
     match _key {
@@ -89,21 +70,19 @@ fn to_get(_key: Option<&str>) {
             let mut _store = KvStore::new();
             match _store.get(_key.to_owned()) {
                 Some(key) => println!("{:?}", key),
-                None => println!("Not available")
+                None => println!("Not available"),
             }
-        },
-        None => unimplemented!()
+        }
+        None => unimplemented!(),
     }
 }
-
 
 fn to_remove(_key: Option<&str>) {
     match _key {
         Some(_key) => {
             let mut _store = KvStore::new();
             _store.remove(_key.to_owned());
-        },
+        }
         None => println!("Not available"),
     }
 }
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,30 @@
-
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq)] 
+#[derive(Debug, PartialEq, Default)]
+
 pub struct KvStore {
-	entry: HashMap<String, String>,
+    entry: HashMap<String, String>,
 }
 
 impl KvStore {
-	// Create a new kvStore hashmap
-	pub fn new() -> KvStore {
-		KvStore {
-			entry: HashMap::new(),
-		}
-	}
-	// Inserts `key` in the  KvStore hashmap
-	pub fn set(&mut self, _key: String, _value: String)  {
-		self.entry.insert(_key, _value);
-	}
+    // Create a new kvStore hashmap
+    pub fn new() -> KvStore {
+        KvStore {
+            entry: HashMap::new(),
+        }
+    }
+    // Inserts `key` in the  KvStore hashmap
+    pub fn set(&mut self, _key: String, _value: String) {
+        self.entry.insert(_key, _value);
+    }
 
-	// Gets `key` from the KvStore hashmap
-	pub fn get(&mut self, _key: String) -> Option<String> {
-		self.entry.get(&_key).cloned()
-	}
+    // Gets `key` from the KvStore hashmap
+    pub fn get(&mut self, _key: String) -> Option<String> {
+        self.entry.get(&_key).cloned()
+    }
 
-	// Removes `key` from the kvStore hashmap
-	pub fn remove(&mut self, _key: String) {
-		self.entry.remove(&_key);
-	}
+    // Removes `key` from the kvStore hashmap
+    pub fn remove(&mut self, _key: String) {
+        self.entry.remove(&_key);
+    }
 }
-
-
